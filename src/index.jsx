d27ef04b4f82f4e8b360ba3f06fe23d5658e4de3
Yoan San <nasub76@gmail.com>
2024-12-02 14:30:08 +0100

    analytics vercel

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics";
+
+Analytics();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
