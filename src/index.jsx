26c1a6cef8d4edf1f03b5d260bef3c207d847795
Yoan San <nasub76@gmail.com>
2024-12-02 14:35:56 +0100

    modif version analytics

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App.jsx";
-import { Analytics } from "@vercel/analytics";
-
-Analytics();
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
 
